ce9ecc70bd6dcb61a09b100a183117d3c33b947e
sopiokvantaliani <skvantaliani@gmail.com>
2023-03-11 21:00:47 -0500

    Adding Roles

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -1,9 +1,7 @@
 package com.cydeo.controller;
 
 import com.cydeo.dto.UserDTO;
-import com.cydeo.entity.User;
-import com.cydeo.service.impl.AbstractMapService;
-import com.cydeo.service.impl.RoleServiceImpl;
+import com.cydeo.service.RoleService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,13 +11,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/user")
 public class UserController {
 
+    private final RoleService roleService; //when we want from other class something we need to inject. Here we inject RoleService;
+
+    public UserController(RoleService roleService) {
+        this.roleService = roleService;
+    }
+
 
     @GetMapping("/create")
     public String createUser (Model model) {
       model.addAttribute("user", new UserDTO()); // We pass here new Object, because initially when we open page we need to see empty fields
-//      model.addAttribute("roles", )
+      model.addAttribute("roles", roleService.findAll() );
       return "/user/create";
-    }
 
+
+    }
 
 }
